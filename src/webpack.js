1861bd0cbb2f8d0b750f1ecafbb3d3961b42e242
Velu S Gautam <i@velusgautam.com>
2020-11-15 23:38:31 +0100

    updated loadble to hooks and webpack4

--- src/webpack.js
+++ src/webpack.js
@@ -9,11 +9,11 @@ function buildManifest(compiler, compilation) {
 
   compilation.chunks.forEach(chunk => {
     chunk.files.forEach(file => {
-      chunk.forEachModule(module => {
+      for (const module of chunk.modulesIterable) {
         let id = module.id;
         let name = typeof module.libIdent === 'function' ? module.libIdent({ context }) : null;
         let publicPath = url.resolve(compilation.outputOptions.publicPath || '', file);
-        
+
         let currentModule = module;
         if (module.constructor.name === 'ConcatenatedModule') {
           currentModule = module.rootModule;
@@ -23,7 +23,7 @@ function buildManifest(compiler, compilation) {
         }
 
         manifest[currentModule.rawRequest].push({ id, name, file, publicPath });
-      });
+      };
     });
   });
 
